bff5f44eecebe079ace54a2ebc07c97dd12cf000
nero <devstory.co.kr@gmail.com>
2020-11-08 13:33:31 +0900

    Update init.sh

--- python/flask/init.sh
+++ python/flask/init.sh
@@ -1,8 +1,8 @@
 # download flask app.py
-curl https://github.com/nero-angela/Scripts/blob/master/python/flask/app.py > ~/app.py
+curl https://github.com/nero-angela/Scripts/blob/master/python/flask/app.py > ./app.py
 
 # install flask
 pip install flask
 
 # run flask
-python ~/app.py
+python ./app.py
