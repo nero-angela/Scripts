32d58b6b3a59c41f75e8ab9f79abb357cf7d8545
nero <devstory.co.kr@gmail.com>
2020-11-08 14:16:09 +0900

    Add template

--- ubuntu/python/flask/init.sh
+++
@@ -1,8 +0,0 @@
-# download flask app.py
-curl https://raw.githubusercontent.com/nero-angela/Scripts/master/python/flask/app.py > ./app.py
-
-# install flask
-pip install flask
-
-# run flask
-python ./app.py
---
+++ ubuntu/python/initFlask.sh
@@ -0,0 +1,8 @@
+# download flask app.py
+curl https://raw.githubusercontent.com/nero-angela/Scripts/master/template/flask.py > ./app.py
+
+# install flask
+pip install flask
+
+# run flask
+python ./app.py
